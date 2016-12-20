45c11e8ecb968e2677dd29fefa661f988e2ac27f
Jianwei <zhujianwei31415@163.com>
2016-12-20 14:12:46 +0800

    change tmp directory

--- Calculate_Scoptest_Top1_Top5.sh
+++ Calculate_Scoptest_Top1_Top5.sh
@@ -17,18 +17,19 @@ echo "$0 $1"
 
 dir=`dirname $(readlink -f $0)`
 L_DATA=$dir/list
+TMPDIR=/tmp/tmp_zjw
 
-mkdir -p ./tmp
+mkdir -p $TMPDIR
 
 echo "Calculating correctly predicted template at family level(Top1, Top5): "
 $dir/calculate_top1_top5.py $OUTPUT $L_DATA/scoptest_family
 
 echo "Calculating correctly predicted template at superfamily level(Top1, Top5): "
-grep -F -v -f  $L_DATA/scoptest_family $OUTPUT > ./tmp/deleted-family
-$dir/calculate_top1_top5.py ./tmp/deleted-family $L_DATA/scoptest_superfamily
+grep -F -v -f  $L_DATA/scoptest_family $OUTPUT > $TMPDIR/deleted-family
+$dir/calculate_top1_top5.py $TMPDIR/deleted-family $L_DATA/scoptest_superfamily
 
 echo "Calculating correctly predicted template at fold level(Top1, Top5): "
-grep -F -v -f $L_DATA/scoptest_superfamily ./tmp/deleted-family > ./tmp/deleted-family-super
-$dir/calculate_top1_top5.py ./tmp/deleted-family-super $L_DATA/scoptest_fold
+grep -F -v -f $L_DATA/scoptest_superfamily $TMPDIR/deleted-family > $TMPDIR/deleted-family-super
+$dir/calculate_top1_top5.py $TMPDIR/deleted-family-super $L_DATA/scoptest_fold
 
-rm -r ./tmp
+rm -r $TMPDIR
