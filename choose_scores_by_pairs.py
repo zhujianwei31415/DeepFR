110d5d613d292fa925a7aa2008b2495ff1298073
Jianwei <zhujianwei31415@163.com>
2017-04-27 01:06:05 +0800

    change output for choose_scores_by_pairs.py

--- choose_scores_by_pairs.py
+++ choose_scores_by_pairs.py
@@ -10,10 +10,9 @@ tmp_dict = {}
 with open(sys.argv[1], 'r') as fin:
     for i in fin:
         cols = i.split()
-        tmp_dict[(cols[0], cols[1])] = cols[2]
+        tmp_dict[(cols[0], cols[1])] = i.strip()
 
 with open(sys.argv[2] , 'r') as fin:
     for i in fin:
         cols = i.split()
-        #print(cols[0], cols[1], tmp_dict[(cols[0], cols[1])])
         print(tmp_dict[(cols[0], cols[1])])
