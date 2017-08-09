16207afc5290d86acafbed717498f601fc57af4a
zjw <zjw@ttic.edu>
2017-08-09 12:22:57 -0500

    update some scripts

--- scripts/training_test/convert_ccmpred_to_image.py
+++ scripts/training_test/convert_ccmpred_to_image.py
@@ -1,13 +1,12 @@
-#! /usr/bin/env python3
-import numpy as np
-import matplotlib.pyplot as plt
-
+#! /usr/bin/env python
 import sys
 
 if len(sys.argv) != 3:
     sys.exit('Usage: %s <coupling> <image_name>' % sys.argv[0])
-print(sys.argv[1:])
 coupling, image_name = sys.argv[1:]
+
+import numpy as np
+import matplotlib.pyplot as plt
 
 matrix = np.loadtxt(coupling)
 plt.imsave(image_name, matrix, cmap=plt.cm.gray_r)
